19809acd17f9181e0fa6eb9c7cb4123968b2b9d4
Zhu JQ <zjq@192.168.0.102>
2020-03-14 21:35:22 +0800

    Format

--- src/TablePropTypes.js
+++ src/TablePropTypes.js
@@ -1,106 +1,109 @@
 import PropTypes from 'prop-types';
 
-const {arrayOf, oneOfType, shape, oneOf, string, func, object, bool, number, exact} = PropTypes;
+const {arrayOf, oneOfType, shape, oneOf, string, func, object, bool, number} = PropTypes;
+
+const leafColumn = shape({
+  // 列id, 默认值造成冲突时才需要指定
+  // 默认值: string name || label
+  id: string,
+
+  // 列名
+  // 或组件: {_rtHeaderProps} => node
+  label: oneOfType([string, func]).isRequired,
+
+  // 列value的record字段名
+  // 或函数: record => value
+  name: oneOfType([string, func]),
+
+  // 列数据类型
+  // 注意: bool一般自定义name函数转为业务string配合select filter使用
+  // 默认值: 'string'
+  // TODO bool
+  type: oneOf(['string', 'number', 'date']),
+
+  // 通过name取值后需要进一步解析为目标type吗?
+  // true为使用该类型的默认格式解析
+  // 或格式串, date需符合 https://date-fns.org/v2.9.0/docs/parse
+  // 或解析函数: value => new value
+  // 默认值: value => value
+  // 典型场景: date
+  parse: oneOfType([bool, string, func]),
+
+  // 显示值时需要进一步格式化吗?
+  // 格式串
+  // 或格式函数: value => string
+  // 默认值:
+  // date: value => yyyy-MM-dd HH:mm:SS
+  // bool: TODO
+  // number: TODO
+  // 其他: value => value
+  // 典型场景: date bool
+  format: oneOfType([string, func]),
+
+  // 显示值时需要进一步组件化吗?
+  // {value, record, _rtCellProps} => node
+  // 默认值: {value} => value
+  // 典型场景: 加样式
+  render: func,
+
+  /*
+    Cell render pipeline:
+    [
+    record
+    - <name> ->
+    value in record
+    - [parse] ->
+
+    value in row
+    - [format] ->
+    value to render
+    ]
+
+    - [render] ->
+    display in UI
+  */
+
+  // 启用工具栏(右)过滤器吗?
+  // true为使用该类型默认的过滤器
+  // 或要使用的过滤器名
+  filter: oneOf([
+    true,
+
+    // 匹配规则: String(value) 包含[忽略大小写] 目标文本, 为string类型的默认过滤器
+    // 适用类型: string, 适用但不推荐: number
+    'text',
+
+    // 匹配规则: value === 目标数值, 为number类型的默认过滤器
+    // 适用类型: number
+    'number',
+
+    // 匹配规则: value 在同一天 目标日期, 为date类型的默认过滤器
+    // 适用类型: date
+    'date',
+
+    // 匹配规则: String(value) === 目标选项
+    // 适用类型: string, 适用但不推荐: number
+    'select',
+  ]),
+
+  // 当filter为select时指定选项, 一般在async模式下使用, local模式将自动生成
+  options: oneOfType([
+    arrayOf(shape({
+      // 作为option的提交value
+      id: number.isRequired,
+      // 作为option的显示内容
+      name: string.isRequired
+    })),
+    arrayOf(shape({
+      id: string.isRequired,
+      name: string.isRequired
+    })),
+  ]),
+});
 
 const TablePropTypes = {
   // 列定义数组, 需要memoized
-  columns: arrayOf(exact({
-    // 列id, 默认值造成冲突时才需要指定
-    // 默认值: string name || label
-    id: string,
-
-    // 列名
-    // 或组件: {_rtHeaderProps} => node
-    label: oneOfType([string, func]).isRequired,
-
-    // 列value的record字段名
-    // 或函数: record => value
-    name: oneOfType([string, func]),
-
-    // 列数据类型
-    // 注意: bool一般自定义name函数转为业务string配合select filter使用
-    // 默认值: 'string'
-    // TODO bool
-    type: oneOf(['string', 'number', 'date']),
-
-    // 通过name取值后需要进一步解析为目标type吗?
-    // true为使用该类型的默认格式解析
-    // 或格式串, date需符合 https://date-fns.org/v2.9.0/docs/parse
-    // 或解析函数: value => new value
-    // 默认值: value => value
-    // 典型场景: date
-    parse: oneOfType([bool, string, func]),
-
-    // 显示值时需要进一步格式化吗?
-    // 格式串
-    // 或格式函数: value => string
-    // 默认值:
-    // date: value => yyyy-MM-dd HH:mm:SS
-    // bool: TODO
-    // number: TODO
-    // 其他: value => value
-    // 典型场景: date bool
-    format: oneOfType([string, func]),
-
-    // 显示值时需要进一步组件化吗?
-    // {value, record, _rtCellProps} => node
-    // 默认值: {value} => value
-    // 典型场景: 加样式
-    render: func,
-
-    /*
-Cell render pipeline:
-[
-  record
-    <name> ->
-  value in record
-    [parse] ->
-
-  value in row
-    [format] ->
-  value to render
-]
-  [render] ->
-display in UI
-     */
-
-    // 启用工具栏(右)过滤器吗?
-    // true为使用该类型默认的过滤器
-    // 或要使用的过滤器名
-    filter: oneOf([
-      true,
-
-      // 匹配规则: String(value) 包含[忽略大小写] 目标文本, 为string类型的默认过滤器
-      // 适用类型: string, 适用但不推荐: number
-      'text',
-
-      // 匹配规则: value === 目标数值, 为number类型的默认过滤器
-      // 适用类型: number
-      'number',
-
-      // 匹配规则: value 在同一天 目标日期, 为date类型的默认过滤器
-      // 适用类型: date
-      'date',
-
-      // 匹配规则: String(value) === 目标选项
-      // 适用类型: string, 适用但不推荐: number
-      'select',
-    ]),
-
-    // 当filter为select时指定选项, 一般在async模式下使用, local模式将自动生成
-    options: oneOfType([
-      arrayOf(shape({
-        // 作为option的提交value
-        id: number.isRequired,
-        // 作为option的显示内容
-        name: string.isRequired
-      })),
-      arrayOf(shape({
-        id: string.isRequired,
-        name: string.isRequired
-      })),
-    ]),
-  })).isRequired,
+  columns: arrayOf(leafColumn).isRequired,
 
   // 查询数据函数, 将开启async模式(即服务端过滤和分页)
   // query => 调API
