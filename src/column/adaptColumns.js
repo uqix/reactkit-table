af9af368e5ce8726860db3263be9b24827fb2040
Zhu JQ <zjq@192.168.0.102>
2020-03-14 16:20:43 +0800

    Refactor out rowExpandColumn

--- src/column/adaptColumns.js
+++ src/column/adaptColumns.js
@@ -1,12 +1,8 @@
-/** @jsx jsx */
-import { jsx, css } from '@emotion/core';
-import { ExpandLess, ExpandMore } from '@material-ui/icons';
 import actionCell from './actionCell';
-import { IconButton } from '@material-ui/core';
-import { Fragment } from 'react';
 import buildColumn from './buildColumn';
 import rowSelectColumn from './rowSelectColumn';
 import rowNumColumn from './rowNumColumn';
+import rowExpandColumn from './rowExpandColumn';
 
 export default function adaptColumns(columns, actions = [], rest) {
   const {rowExpandEnabled, recordNameKey} = rest;
@@ -14,60 +10,7 @@ export default function adaptColumns(columns, actions = [], rest) {
     rowSelectColumn,
     rowNumColumn,
 
-    rowExpandEnabled && {
-      id: '_rowExpand',
-      name: recordNameKey,
-      label: ({
-        _rtHeaderProps: {
-          getToggleAllRowsExpandedProps,
-          isAllRowsExpanded
-        }
-      }) => {
-        const ToggleIcon = isAllRowsExpanded ? ExpandLess : ExpandMore;
-        return (
-          <IconButton
-            size='small'
-            {...getToggleAllRowsExpandedProps()}
-          >
-            <ToggleIcon />
-          </IconButton>
-        );
-      },
-      render: ({
-        _rtCellProps: {
-          cell: {value},
-          row: {
-            canExpand,
-            getToggleRowExpandedProps,
-            depth,
-            isExpanded,
-            xFlat,
-          }
-        }
-      }) => {
-        const ToggleIcon = isExpanded ? ExpandLess : ExpandMore;
-        return (
-          <Fragment>
-            {!xFlat && (
-              <IconButton
-                css={css`
-margin-left: ${depth * 16}px;
-visibility: ${canExpand ? 'visible' : 'hidden'};
-                    `}
-                size='small'
-                {...getToggleRowExpandedProps()}
-              >
-                <ToggleIcon />
-              </IconButton>
-            )}
-
-            <span css={css`vertical-align: middle;`}>
-              {value}
-            </span>
-          </Fragment>
-        );
-      },
-    },
+    rowExpandEnabled && rowExpandColumn(recordNameKey),
 
     ...columns,
 
---
+++ src/column/rowExpandColumn.js
@@ -0,0 +1,62 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
+import { Fragment } from 'react';
+
+const rowExpandColumn = recordNameKey => ({
+  id: '_rowExpand',
+  name: recordNameKey,
+  label: ({
+    _rtHeaderProps: {
+      getToggleAllRowsExpandedProps,
+      isAllRowsExpanded
+    }
+  }) => {
+    const ToggleIcon = isAllRowsExpanded ? ExpandLess : ExpandMore;
+    return (
+      <IconButton
+        size='small'
+        {...getToggleAllRowsExpandedProps()}
+      >
+        <ToggleIcon />
+      </IconButton>
+    );
+  },
+  render: ({
+    _rtCellProps: {
+      cell: {value},
+      row: {
+        canExpand,
+        getToggleRowExpandedProps,
+        depth,
+        isExpanded,
+        xFlat,
+      }
+    }
+  }) => {
+    const ToggleIcon = isExpanded ? ExpandLess : ExpandMore;
+    return (
+      <Fragment>
+        {!xFlat && (
+          <IconButton
+            css={css`
+margin-left: ${depth * 16}px;
+visibility: ${canExpand ? 'visible' : 'hidden'};
+                `}
+            size='small'
+            {...getToggleRowExpandedProps()}
+          >
+            <ToggleIcon />
+          </IconButton>
+        )}
+
+        <span css={css`vertical-align: middle;`}>
+          {value}
+        </span>
+      </Fragment>
+    );
+  },
+});
+
+export default rowExpandColumn;
