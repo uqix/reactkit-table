a3f526b57967ac94e3ddcec8837ab4563582a69f
Zhu JQ <zjq@192.168.0.103>
2020-04-05 12:04:22 +0800

    example, add drawer

--- example/src/App.js
+++ example/src/App.js
@@ -7,6 +7,18 @@ import { zhCN } from '@material-ui/core/locale';
 import { DndProvider } from 'react-dnd';
 import Html5Backend from 'react-dnd-html5-backend';
 import Main from './Main';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const theme = createMuiTheme(
   {
@@ -14,14 +26,80 @@ const theme = createMuiTheme(
   zhCN,
 );
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
 export default function App() {
+  const classes = useStyles();
   return (
     <MrThemeProvider theme={theme}>
       <EmThemeProvider theme={theme}>
         <DndProvider backend={Html5Backend}>
           <BrowserRouter>
-            <CssBaseline />
-            <Main />
+            <div className={classes.root}>
+              <CssBaseline />
+              <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                  <Typography variant="h6" noWrap>
+                    Permanent drawer
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+              <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+                anchor="left"
+              >
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                  {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                      <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider />
+                <List>
+                  {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                      <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Drawer>
+              <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Main />
+              </main>
+            </div>
           </BrowserRouter>
         </DndProvider>
       </EmThemeProvider>
