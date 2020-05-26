b7ce34c79f0e188a3d91199e25d6168cfca68c7f
Zhu JQ <zjq@Zenkies-MacBook-Pro.local>
2020-05-26 11:12:51 +0800

    Remove emotion dependency, rowNumColumn

--- src/column/rowNumColumn.js
+++ src/column/rowNumColumn.js
@@ -1,24 +1,30 @@
-/** @jsx jsx */
-import { jsx, css } from '@emotion/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+
+const useStyles = makeStyles({
+  root: {
+    color: 'gray',
+    cursor: ({xDragRef}) => xDragRef ? 'move' : 'auto'
+  },
+  cell: {
+    width: 20,
+    padding: '8px 0 !important'
+  },
+});
 
 const rowNumColumn = {
   id: '_rowNum',
   label: '#',
-  render: ({_rtCellProps: {cell, xDragRef}}) => (
-    <span
-      css={css`
-color: gray;
-cursor: ${xDragRef ? 'move' : 'auto'};
-          `}
-      ref={xDragRef}
-    >
-      {cell.row.index + 1}
-    </span>
-  ),
-  css: (css`
-width: 20px;
-padding: 8px 0 !important;
-       `),
+  render: ({_rtCellProps: {cell, xDragRef}}) => {
+    const classes = useStyles({xDragRef});
+    return (
+      <span className={classes.root} ref={xDragRef}>
+        {cell.row.index + 1}
+      </span>
+    );
+  },
+  // TODO
+  // css: classes.cell
 };
 
 export default rowNumColumn;
